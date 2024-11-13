96e5a8f51c153e03f26ee589b90e48d3ff3f4172
Jenny Ho <nha.jennyho@gmail.com>
2024-11-13 16:51:37 -0500

    add all input attributes for the form

--- src/components/WarehouseAddForm/WarehouseAddForm.jsx
+++ src/components/WarehouseAddForm/WarehouseAddForm.jsx
@@ -3,26 +3,66 @@ function WarehouseAddForm() {
 
     return (
         <div>
-            <form>
+            <form className="warehouseform">
                 <h2> Warehouse Details </h2>
-                <label>WareHouse Name</label>
-                <input></input>
+                <label htmlfor="warehouseName">WareHouse Name</label>
+                <input
+                    type="text"
+                    name="warehouseName"
+                    placeholder="Warehouse Name"
+                    className= ""
+                ></input>
                 <label>Street Address</label>
-                <input></input>
+                <input
+                    type="text"
+                    name="streetAddress"
+                    placeholder="Street Address"
+                    className= ""
+                ></input>
                 <label>City</label>
-                <input></input>
+                <input
+                    type="text"
+                    name="city"
+                    placeholder="City"
+                    className= ""
+                ></input>
                 <label>Country</label>
-                <input></input>
+                <input
+                    type="text"
+                    name="country"
+                    placeholder="Country"
+                    className= ""
+                ></input>
 
                 <h2>Contact Details</h2>
                 <label>Contact Name</label>
-                <input></input>
+                <input
+                    type="text"
+                    name="contactName"
+                    placeholder="Contact Name"
+                    className= ""
+                ></input>
                 <label>Position</label>
-                <input></input>
+                <input
+                    type="text"
+                    name="position"
+                    placeholder="Position"
+                    className= ""
+                ></input>
                 <label>Phone Number</label>
-                <input></input>
+                <input
+                    type="text"
+                    name="phoneNumber"
+                    placeholder="Phone Number"
+                    className= ""
+                ></input>
                 <label>Email</label>
-                <input></input>
+                <input
+                    type="text"
+                    name="email"
+                    placeholder="Email"
+                    className= ""
+                ></input>
 
             </form>
         </div>
