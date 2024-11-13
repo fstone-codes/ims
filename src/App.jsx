05a02010ccf6a42074fe763c8aedfbce128efbc4
Jenny Ho <nha.jennyho@gmail.com>
2024-11-13 11:57:52 -0500

    add warehouse route, empty return

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   <>
     <BrowserRouter>
       <Routes>
-        <Routes path="/" element={<Homepage/>}/>
+        <Routes path="/" element={<Warehouse/>}/>
       </Routes>
     </BrowserRouter>
   </>
