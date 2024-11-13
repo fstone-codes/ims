8b54350ea05585a36b02d70d95db16416448cdac
fstone <164958630+fstone-codes@users.noreply.github.com>
2024-11-13 11:16:02 -0500

    create warehouse details component with placeholder text and add some initial styling for the layout

---
+++ src/components/WarehouseDetails/WarehouseDetails.jsx
@@ -0,0 +1,27 @@
+import "./WarehouseDetails.scss";
+
+function WarehouseDetails() {
+    return (
+        <section className="wh-details">
+            <div className="wh-details__container wh-details__container--top-left">
+                <h2 className="wh-details__label">WAREHOUSE ADDRESS:</h2>
+                <p className="wh-details__text">
+                    <span className="wh-details__text--span">to be replaced</span>
+                    <span className="wh-details__text--span">to be replaced</span>
+                </p>
+            </div>
+            <div className="wh-details__container">
+                <h2 className="wh-details__label">CONTACT NAME:</h2>
+                <p className="wh-details__text">to be replaced</p>
+                <p className="wh-details__text">to be replaced</p>
+            </div>
+            <div className="wh-details__container">
+                <h2 className="wh-details__label">CONTACT INFORMATION:</h2>
+                <p className="wh-details__text">to be replaced</p>
+                <p className="wh-details__text">to be replaced</p>
+            </div>
+        </section>
+    );
+}
+
+export default WarehouseDetails;
