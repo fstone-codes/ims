b1e1ede8d8e2e321fccc6ac451b30c41644f35d9
Jenny Ho <nha.jennyho@gmail.com>
2024-11-13 15:44:43 -0500

    initital html structure for warehouse add form

---
+++ src/components/WarehouseAddForm/WarehouseAddForm.jsx
@@ -0,0 +1,34 @@
+
+function WarehouseAddForm() {
+
+    return (
+        <div>
+            <form>
+                <h2> Warehouse Details </h2>
+                <label>WareHouse Name</label>
+                <input></input>
+                <label>Street Address</label>
+                <input></input>
+                <label>City</label>
+                <input></input>
+                <label>Country</label>
+                <input></input>
+
+                <h2>Contact Details</h2>
+                <label>Contact Name</label>
+                <input></input>
+                <label>Position</label>
+                <input></input>
+                <label>Phone Number</label>
+                <input></input>
+                <label>Email</label>
+                <input></input>
+
+            </form>
+        </div>
+       
+    )
+}
+
+
+export default WarehouseAddForm
