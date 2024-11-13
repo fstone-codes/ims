8647f146d9944390e2da7613635a41490abe63e3
Jenny Ho <nha.jennyho@gmail.com>
2024-11-13 12:51:29 -0500

    add warehouse routes: edit, add, details

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Warehouse from './pages/Warehouse/Warehouse.jsx'
+import Edit from './pages/Edit/Edit.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +12,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Routes path="/" element={<Warehouse/>}/>
+        <Routes path="/edit" element={<Edit/>}/>
       </Routes>
     </BrowserRouter>
   </>
