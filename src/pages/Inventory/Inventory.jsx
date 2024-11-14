f78b44220c7422703c7e9f3e3ab49303b2df33dd
Shina Ng <ShinaLNYK@gmail.com>
2024-11-14 10:13:24 -0500

    Reset margins and paddings in global.scss

--- src/pages/Inventory/Inventory.jsx
+++ src/pages/Inventory/Inventory.jsx
@@ -1,3 +1,4 @@
+import "./Inventory.scss";
 import { Link } from "react-router-dom";
 import searchIcon from "../../assets/Icons/search-24px.svg";
 import sortIcon from "../../assets/Icons/sort-24px.svg";
@@ -5,75 +6,77 @@ import sortIcon from "../../assets/Icons/sort-24px.svg";
 function Inventory() {
 
     return (
-        <main className="inventory">
-            <h1 className="inventory__title">Inventory</h1>
-            <article className="inventory__CTA-field-container">
-                <div className="field-wrapper">
-                    <img 
-                    className="field-icon" 
-                    src={searchIcon}
-                    alt="Search icon"
-                    />
-                    <input 
-                    className="input-field" 
-                    type="text" 
-                    placeholder="Search..." 
-                    />
-                </div>
-                {/* add onclick handler */}
-                <button className="button">+Add New item</button>
-            </article>
-            {/* <ul className="inventory__table">
-                <li className="inventory__row-entry">
-                    <article className="inventory__col-entry">
-                        <div className="col">
-                            <h4 className="col__header">INVENTORY ITEM</h4>
-                            <img className="col__header-img" src={sortIcon} alt="Sort icon" />
-                        </div>
-                        
-                            <p className="body-small"></p>
-                            <img 
-                            className="table-icon" 
-                            src="" 
-                            alt="Blue right chevron" />
-                       
-                    </article>
-                    <article className="inventory__col-entry">
-                        <div className="col">
-                            <h4 className="col__header"></h4>
-                            <img className="col__header-img" src="" alt="Sort icon" />
-                        </div>
-                        
-                    </article>
-                    <article className="inventory__col-entry">
-                        <div className="col">
-                            <h4 className="col__header"></h4>
-                            <img className="col__header-img" src="" alt="Sort icon" />
-                        </div>
-                        
-                    </article>
-                    <article className="inventory__col-entry">
-                        <div className="col">
-                            <h4 className="col__header"></h4>
-                            <img className="col__header-img" src="" alt="Sort icon" />
-                        </div>
-                        
-                    </article>
-                    <article className="inventory__col-entry">
-                        <div className="col">
-                            <h4 className="col__header"></h4>
-                            <img className="col__header-img" src="" alt="Sort icon" />
-                        </div>
-                        
-                    </article>
-                    <article className="inventory__col-entry inventory__col-entry--last">
-                        <h4 className="table-header"></h4>
-                        <div className="">
+        <main className="main">
+            <section className="inventory">
+                <h1 className="inventory__title">Inventory</h1>
+                <article className="inventory__CTA-field-container">
+                    <div className="field-wrapper">
+                        <input 
+                        className="input-field" 
+                        type="text" 
+                        placeholder="Search..." 
+                        />
+                        <img 
+                        className="field-icon" 
+                        src={searchIcon}
+                        alt="Search icon"
+                        />
+                    </div>
+                    {/* add onclick handler */}
+                    <button className="button">+Add New item</button>
+                </article>
+                <ul className="inventory__table">
+                    <li className="inventory__row-entry">
+                        <article className="inventory__col-entry">
+                            <div className="col">
+                                <h4 className="col__header">INVENTORY ITEM</h4>
+                                <img className="col__header-img" src={sortIcon} alt="Sort icon" />
+                            </div>
+                            <Link>
+                                <p className="body-small"></p>
+                                <img 
+                                className="table-icon" 
+                                src="" 
+                                alt="Blue right chevron" />
+                            </Link>
+                        </article>
+                        <article className="inventory__col-entry">
+                            <div className="col">
+                                <h4 className="col__header"></h4>
+                                <img className="col__header-img" src="" alt="Sort icon" />
+                            </div>
+                            
+                        </article>
+                        <article className="inventory__col-entry">
+                            <div className="col">
+                                <h4 className="col__header"></h4>
+                                <img className="col__header-img" src="" alt="Sort icon" />
+                            </div>
+                            
+                        </article>
+                        <article className="inventory__col-entry">
+                            <div className="col">
+                                <h4 className="col__header"></h4>
+                                <img className="col__header-img" src="" alt="Sort icon" />
+                            </div>
+                            
+                        </article>
+                        <article className="inventory__col-entry">
+                            <div className="col">
+                                <h4 className="col__header"></h4>
+                                <img className="col__header-img" src="" alt="Sort icon" />
+                            </div>
+                            
+                        </article>
+                        <article className="inventory__col-entry inventory__col-entry--last">
+                            <h4 className="table-header"></h4>
+                            <div className="">
 
-                        </div>
-                    </article>
-                </li>
-            </ul> */}
+                            </div>
+                        </article>
+                    </li>
+                </ul>
+            </section>
         </main>
     );
 }
