2d14f5416bdb015be30dcf3762f894af184d3ad6
Gurneer Bedi <gurneer1223@gmail.com>
2024-11-14 13:38:26 -0500

    Working on S24AC-9

--- src/pages/Warehouse/Warehouse.jsx
+++ src/pages/Warehouse/Warehouse.jsx
@@ -1,12 +1,40 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './warehouse.scss';
 
+//hardcoded data 
+const warehouses = [
+    {id: 1, name: 'Manhattan' , address: '503 Broadway, New York, USA', contactName: 'Parmin Aujla' , contactInfo: '+1 (629)-555-0129'},
+    {id: 2, name: 'Manhattan' , address: '503 Broadway, New York, USA', contactName: 'Parmin Aujla' , contactInfo: '+1 (629)-555-0129'}
+];
 
 function Warehouse() {
-
     return (
-        <div>
-
+        <main className = 'main'>
+        <div className = 'warehouse-list'>
+            <div className = 'warehouse-list__header'>
+            <h1 className = 'warehouse-list__heading'>Warehouses</h1>
+            <div className = 'warehouse-list__search-container'> <input type="text"
+            placeholder="Search..."
+            className = "warehouse-list__search" />
+            <button className = 'warehouse-list__add-warehouse-button'>+Add New Warehouse</button>
+            </div>
+            </div>
+            <div className = "warehouse-list__items">
+                {warehouses.map((warehouse) => (
+                    <li key = {warehouse.id} className='warehouse-list__item'>
+                        <Link to = {`/warehouse/${warehouse.id}`} className = "warehouse-list__link">
+                        <h2>{warehouse.name}</h2>
+                        <p>Address {warehouse.address}</p>
+                        <p>Contact Name {warehouse.contactName}</p>
+                        </Link>
+                    </li>
+                ))}
+            </div>
+            
         </div>
-    )
-}
+        </main>
+    );
+};
 
 export default Warehouse
