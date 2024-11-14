5b8d0d119d52f2beac163e00c034fa8c59282352
Jenny Ho <nha.jennyho@gmail.com>
2024-11-14 01:09:45 -0500

    html layout for warehouse add form component

--- src/components/WarehouseAddForm/WarehouseAddForm.jsx
+++ src/components/WarehouseAddForm/WarehouseAddForm.jsx
@@ -1,68 +1,72 @@
 
+
 function WarehouseAddForm() {
 
     return (
         <div>
             <form className="warehouseform">
                 <h2> Warehouse Details </h2>
-                <label htmlfor="warehouseName">WareHouse Name</label>
+                <label htmlFor="warehouseName" class="warehouseform__label-name">WareHouse Name</label>
                 <input
                     type="text"
                     name="warehouseName"
                     placeholder="Warehouse Name"
-                    className= ""
+                    className= "warehouseform__input-name"
                 ></input>
-                <label>Street Address</label>
+                <label htmlFor="streetAdress" class="warehouseform__label-street">Street Address</label>
                 <input
                     type="text"
                     name="streetAddress"
                     placeholder="Street Address"
-                    className= ""
+                    className= "warehouseform__input-street"
                 ></input>
-                <label>City</label>
+                <label htmlFor="city" class="warehouseform__label-city">City</label>
                 <input
                     type="text"
                     name="city"
                     placeholder="City"
-                    className= ""
+                    className= "warehouseform__input-city"
                 ></input>
-                <label>Country</label>
+                <label htmlFor="country" class="warehouseform__label-country">Country</label>
                 <input
                     type="text"
                     name="country"
                     placeholder="Country"
-                    className= ""
+                    className= "warehouseform__input-country"
                 ></input>
 
                 <h2>Contact Details</h2>
-                <label>Contact Name</label>
+                <label htmlFor="contact" class="warehouseform__label-contact">Contact Name</label>
                 <input
                     type="text"
                     name="contactName"
                     placeholder="Contact Name"
-                    className= ""
+                    className= "warehouseform__input-contact"
                 ></input>
-                <label>Position</label>
+                <label htmlFor="position" class="warehouseform__label-position">Position</label>
                 <input
                     type="text"
                     name="position"
                     placeholder="Position"
-                    className= ""
+                    className= "warehouseform__input-position"
                 ></input>
-                <label>Phone Number</label>
+                <label htmlFor="phoneNumber" class="warehouseform__label-phoneNumber">Phone Number</label>
                 <input
                     type="text"
                     name="phoneNumber"
                     placeholder="Phone Number"
-                    className= ""
+                    className= "warehouseform__input-phoneNumber"
                 ></input>
-                <label>Email</label>
+                <label htmlFor="email" class="warehouseform__label-email">Email</label>
                 <input
                     type="text"
                     name="email"
                     placeholder="Email"
-                    className= ""
+                    className= "warehouseform__input-email"
                 ></input>
+
+                <button type="button" class="warehouseform__button-cancel">Cancel</button>
+                <butto type="submit" class="warehouseform__button-add">+ Add Warehouse</butto>
 
             </form>
         </div>
