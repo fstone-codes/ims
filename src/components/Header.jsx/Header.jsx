f0696e04346835043f82c3885238b04f5fbbd3c6
Shina Ng <ShinaLNYK@gmail.com>
2024-11-16 16:44:57 -0500

    Add conditional stylling for warehouses & inventories paths

--- src/components/Header.jsx/Header.jsx
+++ src/components/Header.jsx/Header.jsx
@@ -1,27 +1,41 @@
 import "./Header.scss";
-import instockLogo from "../../assets/logos/InStock-Logo_2x.png"
-import { Link,NavLink } from "react-router-dom";
+import instockLogo from "../../assets/logos/InStock-Logo_2x.png";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
     return (
         <header className="header">
             <nav className="nav">
-                <Link to="/warehouse">
-                    <img 
-                    className="nav__logo"
-                    src={instockLogo} 
-                    alt="InStock logo" 
+                <Link to="/">
+                    <img
+                        className="nav__logo"
+                        src={instockLogo}
+                        alt="InStock logo"
                     />
                 </Link>
                 <div className="nav__button-wrapper">
-                    <NavLink to="/warehouse">
+                    <NavLink
+                        to="/warehouse"
+                        className={({ isActive }) => {
+                            isActive
+                                ? "nav__button nav__button--active"
+                                : "nav__button";
+                        }}
+                    >
                         <button className="nav__button">Warehouses</button>
                     </NavLink>
-                    <NavLink to="/inventory">
+                    <NavLink
+                        to="/inventory"
+                        className={({ isActive }) => {
+                            isActive
+                                ? "nav__button nav__button--active"
+                                : "nav__button";
+                        }}
+                    >
                         <button className="nav__button">Inventory</button>
                     </NavLink>
                 </div>
             </nav>
         </header>
-    )
-};
+    );
+}
